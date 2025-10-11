255bf05f77297e01a99a595a1ce6f31300088926
.Omar <55897834+OKLMARO@users.noreply.github.com>
2025-10-11 13:08:31 +0200

    Mise en place du header

--- philo/philo.h
+++ philo/philo.h
@@ -6,11 +6,39 @@
 /*   By: oamairi <oamairi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/10 12:19:14 by oamairi           #+#    #+#             */
-/*   Updated: 2025/10/10 12:19:45 by oamairi          ###   ########.fr       */
+/*   Updated: 2025/10/11 12:39:22 by oamairi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #ifndef PHILO_H
 # define PHILO_H
+
+# include <pthread.h>
+# include <unistd.h>
+
+typedef struct s_fourchette
+{
+	int					id;
+	pthread_mutex_t		fourchette;
+	struct s_fourchette	*next;
+	struct s_fourchette	*previous;
+}						t_fourchette;
+
+typedef	struct s_philo
+{
+	int					id;
+	pthread_t			philo;
+	struct s_philo		*next;
+	struct s_philo		*previous;
+}						t_philo;
+
+typedef struct s_table
+{
+	t_philo			**philosophe;
+	t_fourchette	**fourchettes;
+}					t_table;
+
+void	ft_putnbr_fd(int n, int fd);
+void	ft_putstr_fd(char *s, int fd);
+
 #endif
--- philo/put.c
+++ philo/put.c
@@ -1,21 +1,47 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   philo.c                                            :+:      :+:    :+:   */
+/*   put.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: oamairi <oamairi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/10 12:20:14 by oamairi           #+#    #+#             */
-/*   Updated: 2025/10/11 13:00:48 by oamairi          ###   ########.fr       */
+/*   Created: 2025/10/11 12:31:50 by oamairi           #+#    #+#             */
+/*   Updated: 2025/10/11 12:39:48 by oamairi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-int	main(int argc, char **argv)
+void	ft_putchar_fd(char c, int fd)
 {
-	if (argc != 5 || argc != 6)
-		return (ft_putstr_fd("NBR ARG ERROR\n", 2), 1);
+	write(fd, &c, 1);
+}
 
-	
+void	ft_putstr_fd(char *s, int fd)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		write(fd, &s[i], sizeof(char));
+		i++;
+	}
+}
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	if (n < 0)
+	{
+		if (n == -2147483648)
+		{
+			ft_putstr_fd("-2147483648", fd);
+			return ;
+		}
+		ft_putchar_fd('-', fd);
+		n = n * -1;
+	}
+	if (n >= 10)
+		ft_putnbr_fd(n / 10, fd);
+	ft_putchar_fd(n % 10 + '0', fd);
 }
